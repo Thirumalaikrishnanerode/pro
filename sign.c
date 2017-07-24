7f76508b771951c83419092ae7dabfe86aeb281a
Thirumalaikrishnanerode <30409064+Thirumalaikrishnanerode@users.noreply.github.com>
2017-07-24 17:58:47 +0530

    Create sign.c

---
+++ sign.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int i;
+scanf("%d",&i);
+if(i>0)
+{
+printf("it is possitive number");
+}
+else if(i==0)
+{
+printf("it is equal to zero");
+}
+else
+{
+printf("it is negative number");
+}
+return 0;
+}
